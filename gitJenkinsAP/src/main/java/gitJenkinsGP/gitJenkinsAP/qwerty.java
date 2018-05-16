808e24f10ec56b53f20518c0319d29ffe964b3a3
AbhimanyuA <divyabhimanyu2016@gmail.com>
2018-05-16 07:51:51 +0530

    my first commit

---
+++ gitJenkinsAP/src/main/java/gitJenkinsGP/gitJenkinsAP/qwerty.java
@@ -0,0 +1,14 @@
+package gitJenkinsGP.gitJenkinsAP;
+
+import org.testng.annotations.Test;
+
+public class qwerty {
+
+	@Test
+	public void fun() {
+		// TODO Auto-generated method stub
+		System.out.println("qwerty function");
+
+	}
+
+}
---
+++ gitJenkinsAP/src/test/java/gitJenkinsGP/gitJenkinsAP/gitJenkinsTestNG.java
@@ -0,0 +1,11 @@
+	package gitJenkinsGP.gitJenkinsAP;
+
+import org.testng.annotations.Test;
+
+public class gitJenkinsTestNG {
+  @Test
+  public void functName() {
+	  
+	  System.out.println("My first commiting and pushing");
+  }
+}
