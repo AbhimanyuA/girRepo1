315060328ae7720dfca461abcb0e7633e15646cc
AbhimanyuA <divyabhimanyu2016@gmail.com>
2018-05-17 12:07:42 +0530

    Interview day

---
+++ gitJenkinsAP/src/test/java/gitJenkinsGP/gitJenkinsAP/gitJenkinsTest.java
@@ -0,0 +1,11 @@
+package gitJenkinsGP.gitJenkinsAP;
+
+import org.testng.annotations.Test;
+
+public class gitJenkinsTest {
+  @Test
+  public void functName() {
+	  
+	  System.out.println("My Interview is today");
+  }
+}
